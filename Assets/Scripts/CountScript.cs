d96cdec931c4c6f992279812a007bee8e9cca38d
Yiyang <liuyiyang0929@gmail.com>
2024-02-22 02:42:43 +0800

    Adjust score counter format

--- Assets/Scripts/CountScript.cs
+++ Assets/Scripts/CountScript.cs
@@ -11,7 +11,7 @@ public class CountScript : MonoBehaviour
     void Update()
     {
         int hitCount = PotHitScript.getScore();
-        scoreCountText.text = "Score: " + hitCount.ToString();
+        scoreCountText.text = "Score: " + hitCount.ToString() + "/ 16";
 
     }
 }
